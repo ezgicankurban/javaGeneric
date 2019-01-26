f47a4457fd92948fffc79c0e62d9cf6e20d82910
ezgicankurban <37628346+ezgicankurban@users.noreply.github.com>
2019-01-26 12:47:15 +0200

    Add files via upload

--- GenericOrnekleri/src/collect/hashset/Runner.java
+++ GenericOrnekleri/src/collect/hashset/Runner.java
@@ -1,15 +1,36 @@
 package collect.hashset;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+
+
 
 public class Runner {
 
 	public static void main(String[] args) {
 		
-		Map<K, V>
+		Map<Integer, String> iller= new HashMap<Integer,String>();
+		iller.put(1, "Adana");
+		iller.put(2, "Adýyaman");
+		iller.put(3, "Afyon");
+		iller.put(4, "Salla");
+		iller.put(5, "Amasya");
+		iller.put(6, "Ankara");
+		iller.put(34, "Ýstanbul");
+		iller.put(8, "Çanakkale");
+		//System.out.println(iller);
+		//while(iller.) iterator özellði olmadýðý için whileile yazýlamaz.
+		for (Entry<Integer, String> temp  : iller.entrySet()) {
+			System.out.println(temp.getKey()+ "-" +temp.getValue());
+			
+		}
+		
+		
+		
 		Set<Integer> liste = new HashSet<Integer>();
 		liste.add(3);
 		liste.add(5);
@@ -29,7 +50,7 @@ public class Runner {
 		
 		Iterator it= liste.iterator();
 		while (it.hasNext()) {
-			System.out.println(it.next());
+			//System.out.println(it.next());
 			
 			
 			
